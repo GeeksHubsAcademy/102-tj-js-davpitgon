52576330e5d04622ec1589f3e42b7e05a394e322
David <d.pitarch@outlook.es>
2021-03-25 00:50:50 +0100

    Edit numPrimo function to check if params are prime numbers and keep it into an array, if number we pass is 1, the return is -1

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,24 @@
 function numPrimo(input) {
- return null;
+    
+    let result = [];
+
+    if ( input == 1 ) {
+        result = -1;
+    }
+
+    for ( let i = 2; i <= input; i ++ ) {
+        let count = 0;
+
+        for ( let j = 1; j <= i; j ++ ) {
+            if ( i % j == 0 )
+            count ++;
+        }
+        if ( count == 2 ) {
+            result.push(i);  
+        }
+
+    }
+    return result;
 }
 
 module.exports = numPrimo;
